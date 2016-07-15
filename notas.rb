20a21900f01595f0734f2a6bb010fc25bdba8c15
codeamac14 <codeamac14@gmail.com>
2016-07-15 17:32:49 -0500

    final Paginacion

---
+++ notas.rb
@@ -0,0 +1,24 @@
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/ul
+http://quirksmode.org/css/css2/display.html
+http://css-tricks.com/the-css-box-model/
+
+Inline elements:
+
+respect left & right margins and padding, but not top & bottom
+cannot have a width and height set
+allow other elements to sit to their left and right.
+Block elements:
+
+respect all of those
+force a line break after the block element
+Inline-block elements:
+
+allow other elements to sit to their left and right
+respect top & bottom margins and padding
+respect height and width
+From W3Schools:
+
+An inline element has no line break before or after it, and it tolerates HTML elements next to it.
+A block element has some whitespace above and below it and does not tolerate any HTML elements next to it.
+An inline-block element is placed as an inline element (on the same line as adjacent content), but it behaves as a block element.
+When you visualize this, it looks like this:
